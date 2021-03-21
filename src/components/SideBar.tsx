0335d5184254a5d62d3183ba5ed7d2e04968fa72
Wilder Amorim <wilderamorim@msn.com>
2021-03-21 07:55:47 -0300

    componentizing the application

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  handleClickButton: (id: number) => void;
+  genres: GenreResponseProps[];
+  selectedGenreId: Number;
+}
+
+export function SideBar({
+  handleClickButton,
+  genres,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
